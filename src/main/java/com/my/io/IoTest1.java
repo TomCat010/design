0af08700a81bcd107ba9908a7a46bdc8462109c8
zhaiwenshun <zhaiws@meyacom.com>
2022-09-20 09:21:10 +0800

    jvm_gc -1

--- src/main/java/com/my/io/IoTest1.java
+++ src/main/java/com/my/io/IoTest1.java
@@ -26,14 +26,34 @@ public class IoTest1 {
     }
     //@Test
     public void ioClose2() {
-        // try with resource 不支持io资源重新赋值 （final 修饰）
+        // try with resource 不支持io资源重新赋值
         try (FileOutputStream fileOutputStream = new FileOutputStream(new File("C:\\Users\\admin\\Desktop\\a.txt"));){
-
-
+            fileOutputStream.write("&& 中文 abc *&^%$#@!".getBytes());
+            //fileOutputStream = null;
         }  catch (IOException e) {
 
             e.printStackTrace();
         }
+    }
+    //ioClose2 反编译后的代码
+    public void ioClose3() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("C:\\Users\\admin\\Desktop\\a.txt"));
+            Throwable throwable = null;
+            if (fileOutputStream != null)
+                if (throwable != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (Throwable throwable1) {
+                        throwable.addSuppressed(throwable1);
+                    }
+                } else {
+                    fileOutputStream.close();
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void close(Closeable... closeables) throws IOException {
